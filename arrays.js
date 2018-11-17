40891c93098278ebe9b16cf8f250b506c317159e
Janette Wallace <jmariewallace2@yahoo.com>
2018-11-17 21:45:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,6 +19,6 @@ function accessElementInArray(array, index) {
   return array[2]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  const array = [1, 2, 3]
+  
   return array.shift()
 }
