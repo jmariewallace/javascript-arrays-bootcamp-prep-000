4a1a103aef55a8afc16e26353a004b348827975d
Janette Wallace <jmariewallace2@yahoo.com>
2018-11-17 21:16:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,10 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
+function addElementToEndOfArray(array, element) {
+  
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
