e05dca957a27f99e7802c6ea0546f593096bf352
Janette Wallace <jmariewallace2@yahoo.com>
2018-11-17 22:04:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,3 +25,9 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length -1)
+}
